f118adc554672b4e9f2f5764844f8245008d554f
Hamza Hedfi <h.hedfi@outlook.com>
2018-09-16 17:50:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -32,9 +32,10 @@ def won?(board)
   if (position_1 == "X" && position_2 == "X" && position_3 == "X") or (position_1 == "O" && position_2 == "O" && position_3 == "O")
     return win_combination # return the win_combination indexes that won.
   else
-    return false
+    win_combination = false
   end
   end
+  win_combination
 end
 
 def full?(board)
